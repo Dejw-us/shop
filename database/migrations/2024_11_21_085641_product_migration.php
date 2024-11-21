0eccde55ac51579fc54a8af5d53c63b38b8c0719
Dawid Ratajczak <dawid.ratajczak06@gmail.com>
2024-11-21 10:12:35 +0100

    Added product migration

---
+++ database/migrations/2024_11_21_085641_product_migration.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  public function up(): void
+  {
+    Schema::create("products", function (Blueprint $table) {
+      $table->id();
+      $table->timestamps();
+      $table->string("name");
+      $table->decimal("price", total: 12, places: 2);
+      $table->integer("stock")->default(0);
+      $table->decimal("discount", total: 12, places: 2);
+    });
+  }
+
+  public function down(): void
+  {
+      Schema::dropIfExists("products");
+  }
+};
