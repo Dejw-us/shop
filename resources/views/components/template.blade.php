180c88393ae27b5d8f0feadeb147577578197fca
Dawid Ratajczak <dawid.ratajczak06+github@gmail.com>
2024-11-22 07:26:43 +0100

    creating dropdown

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>{{ $title ?? "Sklep" }}</title>
   @vite("resources/css/app.css")
+  <script src="https://kit.fontawesome.com/f244f640a4.js" crossorigin="anonymous"></script>
 </head>
 {{ $slot }}
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,25 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Shop</title>
-        @vite("resources/css/app.css")
-    </head>
-    <body class="flex flex-col items-center">
-      <nav class="flex justify-between">
-        <h1 class="text-4xl m-3 p-1">Szybki Sklep</h1>
-        <form action="{{ route("products.search") }}" method="get">
-          <input class="m-3" type="text" placeholder="szukaj produktów" name="search">
-          <button class="text-3xl m-3 bg-green-500 p-2 rounded" type="submit">Szukaj</button>
-        </form>
-      </nav>
-    </body> 
-</html>
+<x-template>
+
+  <body class="flex flex-col items-center">
+    <nav class="flex justify-between">
+      <x-dropdown title="test" class="bg-red-200">
+        <div>
+          test
+        </div>
+        <div>
+          dupa
+        </div>
+      </x-dropdown>
+      <h1 class="text-4xl m-3 p-1">Szybki Sklep</h1>
+      <form action="{{ route("products.search") }}" method="get">
+        <input class="m-3" type="text" placeholder="szukaj produktów" name="search">
+        <button class="text-3xl m-3 bg-green-500 p-2 rounded" type="submit">Szukaj</button>
+      </form>
+    </nav>
+    <script>
+      function dropdown() {
+        document.getElementById("dropdown").classList.toggle("hidden");
+      }
+    </script>
+  </body>
+</x-template>
